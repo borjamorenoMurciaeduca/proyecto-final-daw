e937501fc08f4f565a89d882c04534663775a662
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-03-26 18:48:38 +0100

    Añadido endpoint mostrar inmuebles de perfil. Mejor estructuera añadiendo message y data en las peticiones

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UsuarioInmueble;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
@@ -17,7 +18,13 @@ class AuthController extends Controller {
         $validatedData['password'] = bcrypt($request->password);
         $user = User::create($validatedData);
         $token = $user->createToken('authToken')->plainTextToken;
-        return response()->json(['user' => $user, 'token' => $token], 201);
+        return response()->json([
+            'messsage' => 'Usuario creado con éxito',
+            'data' => [
+                'user' => $user,
+                'token' => $token
+            ]
+        ], 201);
         // } catch (ValidationException $e) {
         //     return response()->json(['errors' => $e->validator->errors()], 422);
         // } catch (Exception $e) {
@@ -35,7 +42,13 @@ class AuthController extends Controller {
             $token = $user->createToken('authToken')->plainTextToken;
             $cookie = cookie('cookie_token', $token, 60 * 24);
 
-            return response()->json(['user' => $user, 'token' => $token], 200);
+            return response()->json([
+                'message' => 'Acceso exitoso!',
+                'data' => [
+                    'user' => $user,
+                    'token' => $token
+                ]
+            ], 200);
             // return response()->json(['user' => $user, 'token' => $token], 200)->withoutCookie($cookie);
         } else {
             return response()->json(['message' => 'Unauthenticated'], 401);
@@ -43,17 +56,14 @@ class AuthController extends Controller {
     }
 
     public function userProfile() {
+        $userId = Auth::id();
+        $user = User::find($userId);
+
+        $usuarioInmuebles = UsuarioInmueble::where('userId', $userId)->get();
         return response()->json([
-            'mensaje' => 'Hola',
-            'numero' => 123,
-            'booleano' => true,
-            'datos' => [
-                'nombre' => 'Juan',
-                'apellido' => 'Pérez',
-                'edad' => 30,
-            ]
+            'message' => 'success',
+            'data' => ['user' => $user, 'usuarioInmuebles' => $usuarioInmuebles]
         ], 200);
-        // return response()->json($request->user(), 200);
     }
 
     public function users() {
@@ -61,13 +71,13 @@ class AuthController extends Controller {
     }
 
     public function logout(Request $request) {
-        // try {
-        $cookie = Cookie::forget('cookie_token');
-        $request->user()->tokens()->delete();
-        Auth::guard('web')->logout();
-        return response()->json(['message' => 'Successfully logged out'], 200)->withCookie($cookie);
-        // } catch (Exception $e) {
-        //     return response()->json('No se ha podido procesar', 404);
-        // }
+        try {
+            $cookie = Cookie::forget('cookie_token');
+            $request->user()->tokens()->delete();
+            Auth::guard('web')->logout();
+            return response()->json(['message' => 'Successfully logged out'], 200)->withCookie($cookie);
+        } catch (\Exception $e) {
+            return response()->json('No se ha podido procesar', 404);
+        }
     }
 }
