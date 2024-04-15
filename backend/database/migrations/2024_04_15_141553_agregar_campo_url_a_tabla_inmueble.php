825039c932a55b25be524822ea2dd1c6e2d225c4
Borja Moreno <11182650@alu.murciaeduca.es>
2024-04-15 16:25:13 +0200

    Añadimos campo url a la tabla inmueble

---
+++ backend/database/migrations/2024_04_15_141553_agregar_campo_url_a_tabla_inmueble.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('inmuebles', function (Blueprint $table) {
+            $table->string('url_imagen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('inmuebles', function (Blueprint $table) {
+            $table->dropColumn('url_imagen');
+        });
+    }
+};
