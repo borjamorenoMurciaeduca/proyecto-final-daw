44cdef12a67ec21e0db33215c080358c5e977b1e
Jorge Balibrea <jorge.balibrea1@gmail.com>
2024-05-08 23:41:08 +0200

    añadido antiguo relieve

--- front/src/pages/register/Register.jsx
+++ front/src/pages/register/Register.jsx
@@ -75,108 +75,125 @@ const Register = () => {
 
   return (
     <Container
-      maxWidth="xs"
+      maxWidth="sm"
       component="main"
       sx={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        minHeight: '90vh',
+        minHeight: '100vh',
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        {t('register-form.title')}
-      </Typography>
-      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-        {error && (
-          <Alert severity="error" sx={{ margin: '10px 0px' }}>
-            {error}
-          </Alert>
-        )}
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              autoComplete="given-name" //???
-              name="username"
-              required
-              fullWidth
-              id="username"
-              label={t('login-form.form.name')}
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="password">
-                {t('login-form.form.password')}
-              </InputLabel>
-              <OutlinedInput
-                id="password"
-                name="password"
-                autoComplete="new-password"
-                type={showPassword ? 'text' : 'password'}
-                label={t('login-form.form.password')}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
+      <Box
+        p={5}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '10px',
+          boxShadow: '2px 2px 45px -15px rgba(0,0,0,0.75)',
+          gap: 2,
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {t('register-form.title')}
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit}>
+          {error && (
+            <Alert severity="error" sx={{ margin: '10px 0px' }}>
+              {error}
+            </Alert>
+          )}
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="given-name" //???
+                name="username"
+                required
+                fullWidth
+                id="username"
+                label={t('login-form.form.name')}
+                autoFocus
               />
-            </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="password">
+                  {t('login-form.form.password')}
+                </InputLabel>
+                <OutlinedInput
+                  id="password"
+                  name="password"
+                  autoComplete="new-password"
+                  type={showPassword ? 'text' : 'password'}
+                  label={t('login-form.form.password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="password_confirmation">
+                  {t('login-form.form.confirm-password')}
+                </InputLabel>
+                <OutlinedInput
+                  id="password_confirmation"
+                  autoComplete="new-password"
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  name="password_confirmation"
+                  label={t('login-form.form.confirm-password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowConfirmPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showConfirmPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="password_confirmation">
-                {t('login-form.form.confirm-password')}
-              </InputLabel>
-              <OutlinedInput
-                id="password_confirmation"
-                autoComplete="new-password"
-                type={showConfirmPassword ? 'text' : 'password'}
-                name="password_confirmation"
-                label={t('login-form.form.confirm-password')}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowConfirmPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
+          <Grid item xs={12} sx={{ mt: 3, mb: 2 }}>
+            <Button type="submit" fullWidth variant="contained">
+              {t('register-form.register')}
+            </Button>
           </Grid>
-        </Grid>
-        <Grid item xs={12} sx={{ mt: 3, mb: 2 }}>
-          <Button type="submit" fullWidth variant="contained">
-            {t('register-form.register')}
-          </Button>
-        </Grid>
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            <Link component={RouterLink} to="/auth" variant="body2">
-              {t('register-form.login')}
-            </Link>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link component={RouterLink} to="/auth" variant="body2">
+                {t('register-form.login')}
+              </Link>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
+        <LanguageSelector />
+        <Copyright />
       </Box>
-      <LanguageSelector />
-      <Copyright sx={{ mt: 5 }} />
     </Container>
   );
 };
